e7fa5736b72e8e2c5ac8dcb22d42068a6d7b7a3e
Kacper <kacperrymkiewicz@gmail.com>
2024-10-26 18:02:44 +0200

    Add Vigenere Cipher app

---
+++ VigenereCipher/VigenereCipher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VigenereCipherApp
+{
+    internal class VigenereCipher
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj tekst do zaszyfrowania:");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Podaj klucz:");
+            int shift = int.Parse(Console.ReadLine());
+        }
+    }
+}
