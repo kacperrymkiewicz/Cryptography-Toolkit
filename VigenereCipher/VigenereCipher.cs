b8589c8db5d89bcacdeec420a634b33d2de7aa52
Kacper <kacperrymkiewicz@gmail.com>
2024-10-26 18:58:20 +0200

    Add Vigenere Cipher implementation

--- VigenereCipher/VigenereCipher.cs
+++ VigenereCipher/VigenereCipher.cs
@@ -8,14 +8,86 @@ namespace VigenereCipherApp
 {
     internal class VigenereCipher
     {
+        public static string Encrypt(string text, string key)
+        {
+            string result = "";
+            int keyIndex = 0;
+
+            foreach (char ch in text)
+            {
+                if (char.IsLetter(ch))
+                {
+                    char offset = char.IsUpper(ch) ? 'A' : 'a';
+                    char keyChar = key[keyIndex % key.Length];
+                    int keyShift = char.IsUpper(keyChar) ? keyChar - 'A' : keyChar - 'a';
+
+                    result += (char)((ch + keyShift - offset) % 26 + offset);
+                    keyIndex++;
+                }
+                else
+                {
+                    result += ch;
+                }
+            }
+
+            return result;
+        }
+
+        public static string Decrypt(string text, string key)
+        {
+            string result = "";
+            int keyIndex = 0;
+
+            foreach (char ch in text)
+            {
+                if (char.IsLetter(ch))
+                {
+                    char offset = char.IsUpper(ch) ? 'A' : 'a';
+                    char keyChar = key[keyIndex % key.Length];
+                    int keyShift = char.IsUpper(keyChar) ? keyChar - 'A' : keyChar - 'a';
+
+                    result += (char)((ch - keyShift + 26 - offset) % 26 + offset);
+                    keyIndex++;
+                }
+                else
+                {
+                    result += ch;
+                }
+            }
+
+            return result;
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj tekst do zaszyfrowania:");
-            string text = Console.ReadLine();
+            while(true)
+            {
+                Console.WriteLine("Wybierz opcję:");
+                Console.WriteLine("1. Szyfruj tekst");
+                Console.WriteLine("2. Odszyfruj tekst");
+                string option = Console.ReadLine();
 
-            Console.WriteLine("Podaj klucz:");
-            int shift = int.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj tekst:");
+                string text = Console.ReadLine();
+
+                Console.WriteLine("Podaj klucz:");
+                string key = Console.ReadLine();
+
+                if (option == "1")
+                {
+                    string encryptedText = Encrypt(text, key);
+                    Console.WriteLine($"Zaszyfrowany tekst: {encryptedText}");
+                }
+                else if (option == "2")
+                {
+                    string decryptedText = Decrypt(text, key);
+                    Console.WriteLine($"Odszyfrowany tekst: {decryptedText}");
+                }
+                else
+                {
+                    Console.WriteLine("Nieprawidłowa opcja");
+                }
+            }
         }
     }
 }
